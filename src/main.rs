8e7675bcec978a5c2cfec972bd12f247fc867b57
Replit user <>
2023-10-10 05:36:36 +0000

    remove log

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
