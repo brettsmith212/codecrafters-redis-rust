35b03a9b99de57e52d0f148fe9ebf03a011b9a0f
Replit user <>
2023-10-10 05:16:46 +0000

    multiple pings

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
-       match stream {
-             Ok(mut _stream) => {
-              println!("accepted new connection");
-               _stream.write(b"+PONG\r\n").unwrap();
-             }
-             Err(e) => {
-                 println!("error: {}", e);
-             }
-         }
-     }
+        match stream {
+            Ok(mut stream) => {
+                println!("accepted new connection");
+                let mut buf = [0; 512];
+                loop {
+                    stream.read(&mut buf).unwrap();
+                    stream.write(b"+PONG\r\n").unwrap();
+                }
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
