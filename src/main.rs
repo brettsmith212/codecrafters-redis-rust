31a035bd061af5c14f8af41cdfdbe38437d6923b
Replit user <>
2023-10-11 04:41:39 +0000

    step 4 test

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
-use std::io::{Read, Write};
-use std::net::TcpListener;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    println!("server ready");
 
-    for stream in listener.incoming() {
+    loop {
+        let stream = listener.accept().await;
         match stream {
-            Ok(mut stream) => {
-                let mut buf = [0; 512];
-                loop {
-                    stream.read(&mut buf).unwrap();
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
+            Ok((mut stream, _)) => {
+                println!("accepted new connection");
+                tokio::spawn(async move {
+                  let mut buf = [0; 512];
+                  loop {
+                    let read_count = stream.read(&mut buf).await.unwrap();
+                    if read_count == 0 {
+                      break;
+                    }
+                    stream.write(b"+PING\r\n").await.unwrap();
+                  }
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
